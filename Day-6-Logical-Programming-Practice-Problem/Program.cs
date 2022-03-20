a2934d6e7b55047b2b9a5c3682615828f136ee2d
ANMOL <anmol999rajput@gmail.com>
2022-03-20 15:01:31 +0530

    Add project files.

---
+++ Day-6-Logical-Programming-Practice-Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_6_Logical_Programming_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
