f6cd5349a21dd6a90c805cc71d540981103084d0
ANMOL <anmol999rajput@gmail.com>
2022-03-20 15:18:44 +0530

    Reverse Number

--- Day-6-Logical-Programming-Practice-Problem/Program.cs
+++ Day-6-Logical-Programming-Practice-Problem/Program.cs
@@ -5,23 +5,18 @@ namespace Day_6_Logical_Programming_Practice_Problem
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int n, i, m = 0, flag = 0;
-            Console.Write("Enter the Number to check Prime: ");
-            n = int.Parse(Console.ReadLine());
-            m = n / 2;
-            for (i = 2; i <= m; i++)
-            {
-                if (n % i == 0)
-                {
-                    Console.Write("Number is not Prime.");
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0)
-                Console.Write("Number is Prime.");
-        }
+        { 
+        int n, reverse = 0, rem;
+        Console.Write("Enter a number: ");      
+       n= int.Parse(Console.ReadLine());     
+       while(n!=0)      
+       {      
+        rem=n%10;        
+        reverse=reverse*10+rem;      
+        n/=10;      
+       }
+    Console.Write("Reversed Number: "+reverse);       
+    }  
 
     }
 }
